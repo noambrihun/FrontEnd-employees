e6a7bfe3e24684f96466b76f8d9151eb7a6bef05
noambrihun <noambrihun1@gmail.com>
2026-04-24 17:58:03 +0300

    redirect to employees page after adding employee

--- src/pages/AddEmployeePage.tsx
+++ src/pages/AddEmployeePage.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function AddEmployeePage() {
     const [name, setName] = useState("");
     const [position, setPosition] = useState("");
     const [salary, setSalary] = useState("");
     const [department, setDepartment] = useState("");
-
+    const navigate = useNavigate();
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
       
@@ -25,6 +26,7 @@ function AddEmployeePage() {
       setSalary("");
       setDepartment("");
       alert("Employee added successfully");
+      navigate("/employees");
     }
 
     return (
