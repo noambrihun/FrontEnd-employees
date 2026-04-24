48adf3f49b122fa74b14a61987cca877b0480a89
noambrihun <noambrihun1@gmail.com>
2026-04-24 16:48:23 +0300

    react initial setup + types for employees project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h1 className='text-3xl font-bold underline text-red-500'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
